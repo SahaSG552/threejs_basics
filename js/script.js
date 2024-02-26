7bbfbefd1610896c581b854c3266b451acd8af8d
SahaSG552 <sahasg552@mail.ru>
2024-02-27 00:04:59 +0400

    Testing OrbitControls first time. Cool =)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as THREE from 'three'
 // Variables
 let cabGlobalParams = {
@@ -12,7 +13,6 @@ function degToRad(deg) {
 }
 
 // Cursor
-
 const cursor = {
     x: 0,
     y: 0
@@ -27,7 +27,7 @@ window.addEventListener('mousemove', (event) => {
 // Scene
 const scene = new THREE.Scene()
 
-// Bounding Box
+// Geometry
 const geometry = new THREE.BoxGeometry(cabGlobalParams.cabWidth, cabGlobalParams.cabHeight, cabGlobalParams.cabDepth)
 const material = new THREE.MeshBasicMaterial({ color: 0x4e4e56, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
@@ -46,41 +46,20 @@ const aspectRatio = screen.width / screen.height
 // Camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 5000)
 // const camera = new THREE.OrthographicCamera(-1000 * aspectRatio, 1000 * aspectRatio, 1000, -1000)
-camera.position.set(0, 0, 0)
+//camera.position.set(0, 0, 0)
+camera.position.x = cabGlobalParams.cabWidth / 2
+camera.position.z = cabGlobalParams.cabDepth + 1000
+camera.position.y = cabGlobalParams.cabHeight / 2
+//camera.rotation.x = degToRad(90)
 scene.add(camera)
-
-// camera.position.set(cabGlobalParams.cabWidth / 2, -1000, cabGlobalParams.cabHeight)
-
-// camera.position.x = cabGlobalParams.cabWidth / 2
-// camera.position.y = -cabGlobalParams.cabDepth - 1000
-// camera.position.z = cabGlobalParams.cabHeight / 2
-// camera.rotation.x = degToRad(90)
-
-// camera.lookAt(mesh.position)
-// scene.add(camera)
-// Camera helper
-/*
-const helper = new THREE.CameraHelper(camera);
-scene.add(helper);
-*/
-
-// const axesHelper = new THREE.AxesHelper(1000)
-// scene.add(axesHelper)
-
-/*
-const size = 5000
-const divisions = 100
-const gridHelper = new THREE.GridHelper(size, divisions,)
-scene.add(gridHelper)
-*/
 
 // Axes
 const axesLength = 1000
 const pointsX = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(axesLength, 0, 0)]
 const pointsZ = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, axesLength, 0)]
 const pointsY = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, axesLength)]
-const lineMaterialX = new THREE.LineBasicMaterial({ color: 0x075300 }) // #4e4e56
-const lineMaterialY = new THREE.LineBasicMaterial({ color: 0xac1616 })
+const lineMaterialX = new THREE.LineBasicMaterial({ color: 0xac1616 })
+const lineMaterialY = new THREE.LineBasicMaterial({ color: 0x075300 }) // #4e4e56
 const lineMaterialZ = new THREE.LineBasicMaterial({ color: 0x1111a6 })
 const linePointsX = new THREE.BufferGeometry().setFromPoints(pointsX)
 const linePointsY = new THREE.BufferGeometry().setFromPoints(pointsY)
@@ -94,8 +73,15 @@ axesGroup.add(yAxe)
 axesGroup.add(zAxe)
 scene.add(axesGroup)
 
-// Renderer
+
+// Canvas
 const canvas = document.querySelector('.webgl')
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.target.set(200, 300, 0)
+controls.update()
+// Renderer
 const renderer = new THREE.WebGL1Renderer({ canvas: canvas })
 renderer.setSize(screen.width, screen.height)
 // renderer.render(scene, camera)
@@ -105,18 +91,23 @@ console.log(`H${cabGlobalParams.cabHeight}_W${cabGlobalParams.cabWidth}_D${cabGl
 
 // Animations
 const loop = () => {
-    // Update Camera
-
-    // camera.position.set(cabGlobalParams.cabWidth / 2 + cursor.x, -cabGlobalParams.cabDepth - 1000, - cursor.y + cabGlobalParams.cabHeight / 2)
-    camera.position.x = Math.sin(-cursor.x * 0.009) * 1000
-    camera.position.z = cabGlobalParams.cabDepth / 2 + Math.cos(-cursor.x * 0.009) * 1000
-    camera.position.y = cabGlobalParams.cabHeight / 2 + cursor.y * 5
-    camera.lookAt(mesh.position)
-    console.log(camera.position)
-    // mesh.rotateZ(degToRad(0.1))
-    // mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), degToRad(0.1))
+    controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(loop)
 }
+
+/*
+const loop = () => {
+    // Update Camera
+    camera.position.x = Math.sin(-cursor.x * 0.009) * 1000
+    camera.position.z = cabGlobalParams.cabDepth / 2 + Math.cos(-cursor.x * 0.009) * 1000
+    camera.position.y = cabGlobalParams.cabHeight + cursor.y * 5
+    camera.lookAt(mesh.position)
+    console.log(camera.position)
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(loop)
+}
+*/
 
 loop()
